ceb2750c2aa50f8e2ec6d1aef66188d56c975378
Mikhail <mishadyshlevich@mail.ru>
2022-07-02 12:34:09 +0300

    add program1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (a > b)
+{
+    max = a;
+    Console.WriteLine("max = " + a); 
+}
+else 
+{
+    max = b;
+    Console.WriteLine("max = " + b);
+}
+
